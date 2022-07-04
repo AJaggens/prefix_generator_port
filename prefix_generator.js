9c1259bb12283254372b937bf7f90c7edb5599b2
ajaggens <misteryugine@gmail.com>
2022-07-04 18:06:08 +0300

    minor edits

--- prefix_generator.js
+++ prefix_generator.js
@@ -1,29 +1,13 @@
 //input args
 const docBody = document.getElementById('outputBody')
-let numbers = []
-let countryCode = prompt('CC')
-let numLength = prompt('length')
 
-let listStart = countryCode * Math.pow(10, numLength)
-let listFinish = +(countryCode + (Math.pow(10, numLength) - 1))
-
-generateNumbers(listStart,listFinish)
-console.log(numbers)
+postInfo()
 
 //generate sub list from args
-function generateNumbers (listStart,listFinish) {
-   for (let i = listStart; i++; i <= listFinish) {
-      numbers.push(i)
-   }
-}
-
-//loop over sub list
-function fetchLoop (numbers) {
-   for (num of numbers) {
-      const currSubNum = num
-      const postUrl = `https://oneapi.infobip.com/1/networks/resolve/${currSubNum}`
-      fetchInfo (postUrl)
-   }
+function postInfo () {
+   let number = prompt('Enter number')
+   let postUrl = `https://oneapi.infobip.com/1/networks/resolve/${number}`
+   fetchInfo (postUrl)
 }
 
 //fetch sequence
@@ -37,11 +21,16 @@ function fetchInfo (postUrl) {
       .then(json => {
          let outputPara = document.createElement('p')
          console.log(json)
-         if (json.mcc == null && json.mnc == null) {
-            outputPara.textContent = `${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC undefined`
+         if (('requestError'in json) == true ) {
+            outputPara.textContent = `${json.requestError.serviceException.text}`
          } else {
-            outputPara.textContent = `${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC ${json.mcc} ${json.mnc}`
+            if (json.mcc == null && json.mnc == null) {
+               outputPara.textContent = `prefix ${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC undefined`
+            } else {
+               outputPara.textContent = `${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC ${json.mcc} ${json.mnc}`
+            }
          }
+         
          docBody.appendChild(outputPara)
       })
 }

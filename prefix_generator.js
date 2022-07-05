28c6f897cc8dcefc81fc1de432900bb46fa9faaf
ajaggens <misteryugine@gmail.com>
2022-07-05 17:51:30 +0300

    more edits

--- prefix_generator.js
+++ prefix_generator.js
@@ -1,13 +1,26 @@
 //input args
 const docBody = document.getElementById('outputBody')
+let numbers = []
 
-postInfo()
 
-//generate sub list from args
-function postInfo () {
-   let number = prompt('Enter number')
-   let postUrl = `https://oneapi.infobip.com/1/networks/resolve/${number}`
-   fetchInfo (postUrl)
+//generate num list
+let countryCode = prompt('enter cc')
+let numLength = prompt('enter length')
+
+let startList = Number(countryCode * (10 ** (numLength - countryCode.length)))
+let finishList = Number(countryCode + ((10 ** (numLength - countryCode.length)) - 1))
+
+generateNumArray()
+
+
+//generate num list
+function generateNumArray () {
+   for (let i = startList; i <= finishList; i = i + 100000000) {
+      let postUrl = `https://oneapi.infobip.com/1/networks/resolve/${i}`
+      fetchInfo(postUrl)
+      console.log(i)
+   }
+   
 }
 
 //fetch sequence
@@ -18,16 +31,16 @@ function fetchInfo (postUrl) {
       })
    
       .then(response => response.json())
+
       .then(json => {
          let outputPara = document.createElement('p')
-         console.log(json)
          if (('requestError'in json) == true ) {
             outputPara.textContent = `${json.requestError.serviceException.text}`
          } else {
             if (json.mcc == null && json.mnc == null) {
-               outputPara.textContent = `prefix ${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC undefined`
+               outputPara.textContent = `prefix ${json.country.prefix}${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC undefined`
             } else {
-               outputPara.textContent = `${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC ${json.mcc} ${json.mnc}`
+               outputPara.textContent = `prefix ${json.country.prefix}${json.networkPrefix} | ${json.network.name} ${json.country.code} | NNC ${json.mcc} ${json.mnc}`
             }
          }
          

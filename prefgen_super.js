f9beb2c5634bfb63123f2c72dd46801f932850d2
AJaggens <misteryugine@gmail.com>
2023-04-04 15:27:17 +0300

    split append seqs added props for mcc mnc localid localname after fetch to list

--- prefgen_super.js
+++ prefgen_super.js
@@ -3,7 +3,7 @@ document.getElementById('')
 
 //imports
 import networksJson from './libs/mno_networks.json' assert {type: 'json'};
-import infBillingJson from './libs/inf_billing.json' assert {type: 'json'};
+import infBillingJson from './libs/inf_billing_0323.json' assert {type: 'json'};
 
 //global vars
 console.log(networksJson);

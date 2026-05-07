5053a7e45a1e172b43495ba2330c8586adb1e360
AJaggens <misteryugine@gmail.com>
2026-05-07 12:49:18 +0300

    updated resources

---
+++ DS_routing_test/gener_watchdog.js
@@ -0,0 +1,9 @@
+//1.import
+//2.sorting to NNC.Arr
+//3.for each array do
+    //3.1 for index do compare index i to index i++
+        //3.1.1 length check
+        //3.1.2 truncate and compare
+        //3.1.3 return result
+    //3.2 return true or false
+//4. return result
